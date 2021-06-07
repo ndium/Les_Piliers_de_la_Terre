97b430a9e1c03a3d030325ba307ba07a3c720593
Antoine <toino1503@gmail.com>
2021-06-07 16:21:15 +0200

    Ajout de la classe Architecte

---
+++ Les Piliers de la Terre/Metier/Architecte.java
@@ -0,0 +1,21 @@
+public class Architecte
+{
+    private static int nbJoueurs = 0;
+    private int numJoueur;
+    
+    private String couleur;
+
+    private int nbDalles = 0;
+
+    public Architecte(String couleur)
+    {
+        this.numJoueur = ++nbJoueurs;
+        this.couleur = couleur;
+    }
+
+    public int    getNumJoueur() { return this.numJoueur; }
+    public int    getNbDalles () { return this.nbDalles;  }
+    public String getCouleur  () { return this.couleur;   }
+
+    public void ajouterDalle() { this.nbDalles++; }
+}
