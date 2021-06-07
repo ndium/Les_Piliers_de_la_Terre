73915e4d6998aaf814208326d3d5a92f7133132e
Jules Latapy <jullatapy@gmail.com>
2021-06-07 15:39:18 +0200

    classe dalle et Parterre +paquetage

--- Les Piliers de la Terre/Metier/Dalle.java
+++ Les Piliers de la Terre/Metier/Dalle.java
@@ -1,14 +1,47 @@
+package Metier ;
+
 public class Dalle {
 
-    public static int Compteur_Nommeur = 'A' ;
+    public static char Compteur_Nommeur = 'A' ;
     
     private int x,y = 0;
 
     private char identifiant ;
 
+    public Dalle[] voisin = new Dalle[6];
+
     public Dalle(int x,int y){
         this.x=x;
         this.y=y;
-        this.identifiant = Compteur_Nommeur++ ;    
+        this.identifiant = ++Dalle.Compteur_Nommeur ;
+    }
+
+    public void ajouterVoisin(Dalle dalle , int index )
+    {
+        try{
+            this.voisin[index] = dalle ;
+        }catch( ArrayIndexOutOfBoundsException ex ){System.out.println("/!\\ Index du voisin en dehors des plages"+identifiant);}
+    }
+
+    public char getID(){
+        return this.identifiant ;
+    }
+
+    public String toString(){
+
+        String s = "" ;
+
+        for (int i=0;i<voisin.length;i++) 
+        {
+            if (voisin(i)!=null){
+                s+= voisin.getID()+"|";
+            }
+            else{
+                s+= " ";
+            }
+            return s ;
+
+        }
+
     }
 }
--- Les Piliers de la Terre/Metier/Parterre.java
+++ Les Piliers de la Terre/Metier/Parterre.java
@@ -1,5 +1,7 @@
 public class Parterre {
 
+    private final int NBR_DALLE = 16 ;
+
     public Parterre(){
         
     }
