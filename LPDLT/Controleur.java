9f60388d11e9bd2a952535af8e5665ce81e5975a
Antoine <toino1503@gmail.com>
2021-06-11 11:41:44 +0200

    rangement de Controleur.java + ajout de continueJeu comme condition de répétition

--- LPDLT/Controleur.java
+++ LPDLT/Controleur.java
@@ -10,32 +10,42 @@ import LPDLT.Metier.Dalle ;
 import LPDLT.Metier.Pilier ;
 import iut.algo.Clavier;
 
-public class Controleur {
+public class Controleur
+{
+    Architecte joueur1;
+    Architecte joueur2;
+    Architecte gagnant = null; // Par défaut, aucun joueur n'est gagnant
+    Architecte joueurActif;
+
+    int  numSommet   = 7;   // Un sommet qui ne peut pas exister
+    char lettreDalle = 'Q'; // Une lettre qui ne peut pas exister
+
+    int tour = 0; // Initialisation du compteur de tour à 0
+
+    ArbitrePlateau metier;
+
+    FrameJeu IHM;
+
+    boolean continueJeu = true;
 
     public Controleur(int mode)
     {
-        Architecte joueur1 = new Architecte("gris");
-        Architecte joueur2 = new Architecte("maron");
-        Architecte joueurActif = joueur1;
-
-        int  numSommet   = 7;   // Un sommet qui ne peut pas exister
-        char lettreDalle = 'Q'; // Une lettre qui ne peut pas exister
-
-        int tour = 0;
+        this.joueur1     = new Architecte("gris");
+        this.joueur2     = new Architecte("maron");
+        this.joueurActif = joueur1;
         
-        ArbitrePlateau metier = new ArbitrePlateau(this);
+        this.metier = new ArbitrePlateau(this);
 
-        FrameJeu IHM = new FrameJeu( this, Dalle.ensembleDalle, Pilier.ensemblePilier );
+        this.IHM = new FrameJeu( this, Dalle.ensembleDalle, Pilier.ensemblePilier );
         
-        while ( !joueur1.gagner() && !joueur2.gagner() )// && Pilier.nbPilierMax > 0)
+        while ( continueJeu )
         {
-            System.out.println( metier.toString() );
-
-            if ( joueurActif == joueur1 ) { tour++; }
+            if ( joueurActif == joueur1 ) { tour++; } // Augmentation du tour quand on retombe sur le joueur 1
+            
             System.out.println( "---------- Tour n°" + tour + " ----------" );
 
             do
-            { 
+            {
                 IHM.maj();
 
                 // Annonce du joueur actif + demande de dalle sur laquelle jouer
@@ -46,7 +56,7 @@ public class Controleur {
                 System.out.println( "\nSur quel sommet voulez vous jouer de [0-5] ?" );
                 numSommet = Clavier.lire_int();
             } 
-            while ( !( metier.ajouterPilier( lettreDalle, numSommet, joueurActif.getCouleur() ) ) );//&& Pilier.nbPilierMax > 0 ) );
+            while ( !( metier.ajouterPilier( lettreDalle, numSommet, joueurActif.getCouleur() ) ) );
             
             System.out.println( "\nPilier construit !! Il en reste " + Pilier.nbPilierMax + " à poser." );
 
@@ -54,39 +64,36 @@ public class Controleur {
             if ( joueurActif == joueur1 ) { joueurActif = joueur2; }
             else                          { joueurActif = joueur1; }
         }
-        /*
-        condition de fin comme R4
-            metier.getAction(joueurActif.getAction());
-            metier.toString();*/
-        
-        public void verificationFinJeu()
-        {
-            // Vérification du nombre de Dalle de chaque joueur
-            if( joueur1.getNbDalle() >= 9 ) { arretJeu = true; gagnant = joueur1; }
-            if( joueur2.getNbDalle() >= 9 ) { arretJeu = true; gagnant = joueur2; }
-
-            // Vérification du nombre de Piliers construits
-            if( Piler.nbPilierConstruit <= 0 )
-            {
-                arretJeu = true;
-                
-                // Contrôle du joueur qui a le plus de dalle
-                if( joueur1.getNbDalle() > joueur2.getNbDalle() ) { gagnant = joueur1; } // Cas où le joueur1 a le plus de Dalles
-                else
-                {
-                    if( joueur1.getNbDalle() < joueur2.getNbDalle() ) { gagnant = joueur2; } // Cas où le joueur2 a le plus de Dalles
-                    else // Cas d'égalité en nombre de Dalles possédées
-                    {
-                        if( joueur1.getNbPilierDetruit() > joueur2.getNbPilierDetruit() ) { gagnant = joueur1; } // Cas où le joueur1 a détruit le plus de Piliers
-                        else
-                        {
-                            if( joueur1.getNbPilierDetruit() < joueur2.getNbPilierDetruit() ) { gagnant = joueur2; } // Cas où le joueur2 a détruit le plus de Piliers
-                        }
-                    }
-                } // Si à la fin de cette boucle arretJeu == true mais gagnant == null, c'est qu'il y a égalité parfaite
-            }
-        }
     }
+        
+    /*public void verificationFinJeu()
+    {
+        // Vérification du nombre de Dalle de chaque joueur
+        if( joueur1.getNbDalle() >= 9 ) { continueJeu = false; gagnant = joueur1; }
+        if( joueur2.getNbDalle() >= 9 ) { continueJeu = false; gagnant = joueur2; }
+
+        // Vérification du nombre de Piliers construits
+        if( Piler.nbPilierConstruit <= 0 )
+        {
+            continueJeu = false;
+            
+            // Contrôle du joueur qui a le plus de dalle
+            if( joueur1.getNbDalle() > joueur2.getNbDalle() ) { gagnant = joueur1; } // Cas où le joueur1 a le plus de Dalles
+            else
+            {
+                if( joueur1.getNbDalle() < joueur2.getNbDalle() ) { gagnant = joueur2; } // Cas où le joueur2 a le plus de Dalles
+                else // Cas d'égalité en nombre de Dalles possédées
+                {
+                    if( joueur1.getNbPilierDetruit() > joueur2.getNbPilierDetruit() ) { gagnant = joueur1; } // Cas où le joueur1 a détruit le plus de Piliers
+                    else
+                    {
+                        if( joueur1.getNbPilierDetruit() < joueur2.getNbPilierDetruit() ) { gagnant = joueur2; } // Cas où le joueur2 a détruit le plus de Piliers
+                    }
+                }
+            } // Si à la fin de cette boucle ( continueJeu == true ) mais ( gagnant == null ), c'est qu'il y a égalité parfaite
+        }
+    }*/
+
     public static void main(String[] args) {
         new Controleur( 0 );
     }
